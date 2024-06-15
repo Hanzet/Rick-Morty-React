fa5b56bb4f515faf04ea9fb4f523e170a043c346
Jonathan Orrego Montoya <jonathan.orrego@ucp.edu.co>
2024-06-15 18:17:50 -0500

    Spinner

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import imageRickMorty from './img/rick-morty.png';
 import './App.css';
-import { useState } from 'react'
+import { useState } from 'react';
 import Characters from './components/Characters';
 
-  function App() {
+function App() {
+  const [characters, setCharacters] = useState(null);
+  const [loading, setLoading] = useState(false); // Estado de carga
 
-    const [characters, setCharacters] = useState(null);
-    const reqApi = async () => {
-      const api = await fetch("https://rickandmortyapi.com/api/character");
-      const characterApi = await api.json();
+  const reqApi = async () => {
+    setLoading(true); // Establece el estado de carga a true antes de la solicitud
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
 
+    setTimeout(() => {
       setCharacters(characterApi.results);
+      setLoading(false); // Establece el estado de carga a false después de recibir los datos
+    }, 1000); // Simula un retraso de 1 segundo
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
+        {loading && <span className="loader"></span>} {/* Muestra el spinner si loading es true */}
         {characters ? (
-          <Characters characters={characters} setCharacters={setCharacters}/>
+          <Characters characters={characters} setCharacters={setCharacters} />
         ) : (
           <>
-          <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
-          <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
           </>
         )}
       </header>
